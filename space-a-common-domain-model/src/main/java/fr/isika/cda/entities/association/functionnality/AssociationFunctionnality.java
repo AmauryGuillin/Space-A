17c4e4dde26f8a7615fe3f5d3e43a14008dc5785
AmauryGuillin <amauryg@hotmail.fr>
2023-04-03 21:19:44 +0200

    mise a jour persistence xml spring

--- space-a-common-domain-model/src/main/java/fr/isika/cda/entities/association/functionnality/AssociationFunctionnality.java
+++ space-a-common-domain-model/src/main/java/fr/isika/cda/entities/association/functionnality/AssociationFunctionnality.java
@@ -10,7 +10,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="Association_Fonctionnality")
-public class AssociationFonctionnality {
+public class AssociationFunctionnality {
 	
 	@Id
 	@GeneratedValue
