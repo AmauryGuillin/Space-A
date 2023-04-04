6f48b6f4c71fb3fa9b3d38374b9c6511d796a99d
AmauryGuillin <amauryg@hotmail.fr>
2023-04-04 17:57:31 +0200

    ajout d'une classe controlleur pour la navbar et tous les liens de redirection

---
+++ space-a-javaee-client-app/src/main/java/fr/isika/cda/presentation/beans/navigation/NavController.java
@@ -0,0 +1,26 @@
+package fr.isika.cda.presentation.beans.navigation;
+
+import java.io.IOException;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+import fr.isika.cda.presentation.utils.WebUiTools;
+
+@ManagedBean
+@ApplicationScoped
+public class NavController {
+
+	public void assoForm() {
+		WebUiTools.redirectToView("/assoManagement/assoForm.xhtml");
+	}
+
+	public void listAllAsso() {
+		WebUiTools.redirectToView("/assoManagement/listAllAsso.xhtml");
+	}
+
+	public void users() throws IOException {
+		WebUiTools.redirectToView("/usersManagement/users.xhtml");
+	}
+
+}
