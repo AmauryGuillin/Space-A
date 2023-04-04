7b294a9ea787f5065dbc74862bf823addf386664
Manuela Raimbault <mr.raimbault@gmail.com>
2023-04-04 22:01:16 +0200

    tests userInfos

---
+++ space-a-javaee-client-app/src/main/java/fr/isika/cda/presentation/beans/users/ShowUserController.java
@@ -0,0 +1,45 @@
+package fr.isika.cda.presentation.beans.users;
+
+import javax.annotation.ManagedBean;
+import javax.inject.Inject;
+
+import fr.isika.cda.data.repositories.users.UserAccountRepository;
+import fr.isika.cda.entities.users.UserAccount;
+
+@ManagedBean
+public class ShowUserController {
+	
+	@Inject
+	private UserAccountRepository userAccountRepo;
+
+	private Long userId;
+	private UserAccount userAccount;
+	
+	public void getOneUser() {
+		userAccount = userAccountRepo.findByOneId(userId);
+	}
+
+	public UserAccountRepository getUserAccountRepo() {
+		return userAccountRepo;
+	}
+
+	public void setUserAccountRepo(UserAccountRepository userAccountRepo) {
+		this.userAccountRepo = userAccountRepo;
+	}
+
+	public UserAccount getUserAccount() {
+		return userAccount;
+	}
+
+	public void setUserAccount(UserAccount userAccount) {
+		this.userAccount = userAccount;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+		
+	
+	
+	
+}
