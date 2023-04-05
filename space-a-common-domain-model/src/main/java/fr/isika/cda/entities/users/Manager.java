0dd1ad2e2ecea27ad09aa5e28f1c50d4c34ccd7c
Manuela Raimbault <mr.raimbault@gmail.com>
2023-04-05 10:46:26 +0200

    dataControlUser

--- space-a-common-domain-model/src/main/java/fr/isika/cda/entities/users/Manager.java
+++ space-a-common-domain-model/src/main/java/fr/isika/cda/entities/users/Manager.java
@@ -1,6 +1,7 @@
 package fr.isika.cda.entities.users;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,6 +19,7 @@ public class Manager {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(nullable = false, unique = true)
 	private String matricule;
 	
 	@ManyToOne(cascade = CascadeType.ALL)
